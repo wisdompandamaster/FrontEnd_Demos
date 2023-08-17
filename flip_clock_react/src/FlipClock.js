3cf5703dd1805e93e755968c7b1826523f0e931b
wisdompandamaster <2532569059@qq.com>
2023-08-18 07:03:42 +0800

    feat(FlipCard):complete the card with animation

--- flip_clock_react/src/FlipClock.js
+++ flip_clock_react/src/FlipClock.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./FlipClock.css";
 
 const FlipClock = () => {
+  useEffect(() => {
+    const startNumber = 9;
+    const flipCard = document.querySelector(".flip-card");
+    const top = document.querySelector(".top");
+    const bottom = document.querySelector(".bottom");
+
+    // 创造翻盖元素
+    const topFlip = document.createElement("div");
+    const bottomFlip = document.createElement("div");
+    topFlip.classList.add("top-flip");
+    bottomFlip.classList.add("bottom-flip");
+
+    // 设置初始数字
+    top.textContent = startNumber;
+    bottom.textContent = startNumber;
+    topFlip.textContent = startNumber;
+    bottomFlip.textContent = startNumber - 1;
+
+    topFlip.addEventListener("animationstart", (e) => {
+      top.textContent = startNumber - 1;
+    });
+    topFlip.addEventListener("animationend", (e) => {
+      topFlip.remove();
+    });
+    bottomFlip.addEventListener("animationend", (e) => {
+      bottom.textContent = startNumber - 1;
+      bottomFlip.remove();
+    });
+
+    // 添加翻盖元素
+    flipCard.append(topFlip, bottomFlip);
+  }, []);
+
   return (
     <div className="flip-card">
-      <div className="top">4</div>
-      <div className="bottom">4</div>
+      <div className="top">5</div>
+      <div className="bottom">5</div>
     </div>
   );
 };
