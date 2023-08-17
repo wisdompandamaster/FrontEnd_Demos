4a64b1267e45bacaff915a2aca60dca928cfd7ad
wisdompandamaster <2532569059@qq.com>
2023-08-18 05:45:29 +0800

    feat(flip-card):complete a card show a number with top and bottom

---
+++ flip_clock_react/src/FlipClock.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./FlipClock.css";
+
+const FlipClock = () => {
+  return (
+    <div className="flip-card">
+      <div className="top">4</div>
+      <div className="bottom">4</div>
+    </div>
+  );
+};
+
+export default FlipClock;
