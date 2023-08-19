4dff110d2d843b358b53349f12be5398a20a8ac0
wisdompandamaster <2532569059@qq.com>
2023-08-19 11:37:40 +0800

    feat(FlipClock):change the folder name

--- flip_clock_react/src/index.js
+++
@@ -1,8 +0,0 @@
-//创建根实例
-import React from "react";
-import { createRoot } from "react-dom/client";
-import FlipClock from "./FlipClock";
-// 挂载app组件
-const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<FlipClock />);
