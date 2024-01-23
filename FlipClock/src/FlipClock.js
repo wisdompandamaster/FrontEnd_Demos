2123ffd559fe2e54e72e1ea872b6fe51f54aa786
wisdompandamaster <2532569059@qq.com>
2024-01-24 05:16:50 +0800

    add setting mode

--- FlipClock/src/FlipClock.js
+++ FlipClock/src/FlipClock.js
@@ -1,16 +1,73 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./FlipClock.css";
 
+const Setting = (props) => {
+  let img_src = props.passed
+    ? "./assets/logo_red.png"
+    : "./assets/logo_white.png";
+
+  const handleOnKeyDown = (e) => {
+    // YYYY-MM-DD的正则表达式
+    let dateRegex = /^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[1-2]\d|3[0-1])$/;
+    if (e.keyCode == 13) {
+      console.log(e.target.value);
+      let date = e.target.value;
+      // 添加正则表达
+      if (dateRegex.test(date)) {
+        localStorage.setItem("ddl", date);
+        props.setValue(date);
+        e.target.value = "";
+      } else {
+        e.target.value = "";
+        e.target.placeholder = "YYYY-MM-DD";
+      }
+    }
+  };
+
+  const formatDate = (date) => {
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    return year + "-" + month + "-" + day;
+  };
+
+  return (
+    <div className="setting">
+      <img src={img_src} />
+      <input
+        className="ddl"
+        placeholder={formatDate(new Date(props.value))}
+        onKeyDown={handleOnKeyDown}
+      ></input>
+    </div>
+  );
+};
+
 const FlipClock = () => {
+  // 初始设置为后两天,如果本地没有存储的话
+  const [ddl, setDdl] = useState(
+    new Date(
+      localStorage.getItem("ddl")
+        ? localStorage.getItem("ddl")
+        : new Date().setDate(new Date().getDate() + 2)
+    )
+  );
+
+  const [passed, setPassed] = useState(false);
+
   useEffect(() => {
-    const countToDate = new Date("2024-01-28");
+    const countToDate = ddl;
+
     let previousTimeBetweenDates;
 
     // 250ms 刷新一次
-    setInterval(() => {
+    const interval = setInterval(() => {
       const currentDate = new Date();
       const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
-      flipAllCards(timeBetweenDates);
+      if (timeBetweenDates < 0) {
+        setPassed(true);
+      }
+      flipAllCards(Math.abs(timeBetweenDates));
       previousTimeBetweenDates = timeBetweenDates;
     }, 250);
 
@@ -73,59 +130,73 @@ const FlipClock = () => {
       // 添加翻盖元素
       flipCard.append(topFlip, bottomFlip);
     }
-  }, []);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ddl]);
+
+  const setValue = (date) => {
+    setDdl(new Date(date));
+    if (new Date(date) - new Date() > 0) {
+      setPassed(false);
+    }
+  };
 
   return (
-    <div className="container">
-      <div className="container-segment">
-        <div className="label">Days</div>
-        <div className="flip-card" data-days-huns>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
+    <>
+      <Setting setValue={setValue} value={ddl} passed={passed} />
+      <div className="container">
+        <div className="container-segment">
+          <div className="label">Days</div>
+          <div className="flip-card" data-days-huns>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
+          <div className="flip-card" data-days-tens>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
+          <div className="flip-card" data-days-ones>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
         </div>
-        <div className="flip-card" data-days-tens>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
+        <div className="container-segment">
+          <div className="label">Hours</div>
+          <div className="flip-card" data-hours-tens>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
+          <div className="flip-card" data-hours-ones>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
         </div>
-        <div className="flip-card" data-days-ones>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
+        <div className="container-segment">
+          <div className="label">Minutes</div>
+          <div className="flip-card" data-minutes-tens>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
+          <div className="flip-card" data-minutes-ones>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
+        </div>
+        <div className="container-segment">
+          <div className="label">Seconds</div>
+          <div className="flip-card" data-seconds-tens>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
+          <div className="flip-card" data-seconds-ones>
+            <div className="top">0</div>
+            <div className="bottom">0</div>
+          </div>
         </div>
       </div>
-      <div className="container-segment">
-        <div className="label">Hours</div>
-        <div className="flip-card" data-hours-tens>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
-        </div>
-        <div className="flip-card" data-hours-ones>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
-        </div>
-      </div>
-      <div className="container-segment">
-        <div className="label">Minutes</div>
-        <div className="flip-card" data-minutes-tens>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
-        </div>
-        <div className="flip-card" data-minutes-ones>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
-        </div>
-      </div>
-      <div className="container-segment">
-        <div className="label">Seconds</div>
-        <div className="flip-card" data-seconds-tens>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
-        </div>
-        <div className="flip-card" data-seconds-ones>
-          <div className="top">0</div>
-          <div className="bottom">0</div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
